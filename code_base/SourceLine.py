4e9f98c99d11ade44ac41b4e4ff63dc9f71e0762
Samuel Atcheson <atcheson@usc.edu>
2021-06-08 20:21:55 -0700

    added 4cs overlay

--- code_base/SourceLine.py
+++ code_base/SourceLine.py
@@ -3,19 +3,14 @@ class SourceLine:
     def __init__(self, l, i, t):
         self.indents = i
         self.line_num = l
-        self.type = t
         self.data = []
 
     # add code fragment to the source code line
     def append(self, fragment):
         self.data.append(fragment)
 
-    def process(self):
-        line = self.indents * '\t'
-        for block in self.data:
-            if block.get_type() == 'delimiter':
-                if block.get_delimiter_type() == 'end-condition':
-                    line += ':'
-            else:
-                line += block.get_data() + ' '
-        return line
+    def get_indents(self):
+        return self.indents
+
+    def get_data(self):
+        return self.data
