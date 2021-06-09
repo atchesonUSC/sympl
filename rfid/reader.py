096ba9a16a6d09ca8fd0cc70bc7c2e68bebaf593
Samuel Atcheson <atcheson@usc.edu>
2021-06-09 04:23:01 +0100

    modified reader

--- rfid/reader.py
+++ rfid/reader.py
@@ -1,64 +1,56 @@
 import os
+import signal
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
 
 
-# def setup_pins():
-#     # Specify pin numbering system
-#     # BOARD = 1, 2, 3, etc.
-#     # BCM = GPIO1, GPIO7, etc.
-#     GPIO.setmode(GPIO.BOARD)
-#
-#     # Disable pin configuration warnings
-#     #GPIO.setwarnings(False)
-#
-#     # Setup output channel
-#     GPIO.setup(chan, GPIO.OUT, initial=GPIO.HIGH)
-
-
-# def select_device(rfid):
-#     # Set the channel LOW
-#     GPIO.output(chan, GPIO.LOW
-#     time.sleep(0.1)
-#
-#     # Read from the sensor
-#     code,text = rfid.read()
-#     print(code)
-#     print(text)
-#
-#     # Set the channel HIGH
-#     GPIO.output(chan, GPIO.HIGH)
-#     time.sleep(0.1)
-
-
 """
-Readers
-=======
-* rfid_0 -> GPIO_8 -> device_0
-* rfid_2 -> GPIO_6 -> device_2
+============================================
+*                 Readers                  *
+============================================
+*
+* rfid_0 -> GPIO_8 (physical 24) -> device_0
+* rfid_1 -> GPIO_7 (physical 26) -> device_1
+* rfid_2 -> GPIO_6 (physical 31) -> device_2
+*
+--------------------------------------------
 """
 
+
+def exit_handler(sig, frame):
+    print('\n[Exit Signal Received]')
+    GPIO.cleanup()
+    os._exit(0)
 
 def main():
-    rfid_0 = SimpleMFRC522()
-    rfid_2 = SimpleMFRC522()
+    # Setup readers
+    rfid_0 = SimpleMFRC522(dev=0)
+    rfid_1 = SimpleMFRC522(dev=1)
+    rfid_2 = SimpleMFRC522(dev=2)
 
     try:
-        rfid = int(input('RFID: '))
-        if rfid == 0:
+        rfid = input('RFID: ')
+        if rfid == '0':
             code, text = rfid_0.read()
             print('Code: {}\nText: {}'.format(code, text))
-        elif rfid == 2:
+        elif rfid == '1':
+            code, text = rfid_1.read()
+            print('Code: {}\nText: {}'.format(code, text))
+        elif rfid == '2':
             code, text = rfid_2.read()
             print('Code: {}\nText: {}'.format(code, text))
+        elif rfid == 'a':
+            code0, text0 = rfid_0.read()
+            code1, text1 = rfid_2.read()
+            print('Code0: {}\nText0: {}'.format(code0, text0))
+            print('Code1: {}\nText1: {}'.format(code1, text1))
         else:
             print('Try again...')
-    except KeyboardInterrupt:
-        print('Interrupted')
     finally:
         GPIO.cleanup()
 
 
 if __name__ == '__main__':
+    signal.signal(signal.SIGINT, exit_handler)
     main()
 
